cd939684a81c80813bda7c84b9e10e4145215710
ThisisSud <82948338+HeythisisSud@users.noreply.github.com>
2025-04-15 23:25:44 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -7,7 +7,6 @@ router.get('/', bookController.getAllBooks);
 router.get('/:isbn', bookController.getBooksbyId);
 router.patch('/:isbn', bookController.editBooksbyId);
 router.delete('/:isbn', bookController.deleteBooksbyId);
-router.patch('/:isbn', bookController.editBooksbyId);
 router.post('/',bookController.postBooks)
 
 
