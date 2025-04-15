f631b3359cbaf38a83a8cbe523eb92ce76e1d553
ThisisSud <82948338+HeythisisSud@users.noreply.github.com>
2025-04-16 00:45:37 +0530

    Update bookModels.js

--- models/bookModels.js
+++ models/bookModels.js
@@ -1,13 +1,3 @@
-
-// const client = new pg.Client({
-//     user:process.env.user,
-//     host: process.env.host,
-//     database:process.env.client,
-//     password:process.env.password,
-//     port:process.env.port,
-// })
-// client.connect()
-
 const { Client } = require('pg');
 
 const client = new Client({
@@ -16,46 +6,49 @@ const client = new Client({
     rejectUnauthorized: false  // required for Render
   }
 });
-
+//Connection to postgres
 client.connect()
   .then(() => console.log("Connected to PostgreSQL"))
   .catch(err => console.error("Connection error", err.stack));
 
 
 
-
+//Database query to get all books
 
 exports.getAllBooks=() => {
-    return new Promise((resolve, reject) => {
+    return new Promise((res, rej) => {
         client.query("Select * FROM library", (err,resp)=>{
             if(err){
                 console.error("Error executing query",err.stack);
-                reject(err);
+                rej(err);
             }else{
                 const response=resp.rows
                
-                resolve(response);
+                res(response);
 
             }  
         })})}
 
+//Database query to get book with specific id.
 
 
 exports.getBooksbyId=(isbn) => {
-  return new Promise((resolve, reject) => {
+  return new Promise((res, rej) => {
 client.query('SELECT * from library where isbn=$1',[isbn], (err,resp)=>{
     if(err){
         console.error("Error executing query",err.stack);
     }else{
         const response=resp.rows
-        resolve(response)
+        res(response)
     }
   
 })})}
 
+//Database query to edit book with specific id.
+
 
 exports.editBooksbyId=(title,author,genre,availability,newIsbn,isbn)=>{
-    return new Promise((resolve, reject) => {
+    return new Promise((res, rej) => {
         client.query('UPDATE library SET title = COALESCE($1, title),author = COALESCE($2, author),genre = COALESCE($3, genre),availability = COALESCE($4, availability),isbn = COALESCE($5, isbn) WHERE isbn = $6',[title,author,genre,availability,newIsbn,isbn], (err,resp)=>{
             if(err){
                 console.error("Error executing query",err.stack);
@@ -63,48 +56,47 @@ exports.editBooksbyId=(title,author,genre,availability,newIsbn,isbn)=>{
                 const response={
                     Message:'Success'
                 }
-                resolve(response)
+                res(response)
             }
-
-
-
     })
-
-
-
 })}
 
 
+
+//Database query to delete book with specific id.
+
+
 exports.deleteBooksbyId=(isbn)=>{
-    return new Promise((resolve, reject) => {
+    return new Promise((res, rej) => {
 
 
     client.query(`DELETE from library where isbn=$1`,[isbn], (err,resp)=>{
         if(err){
             console.error("Error executing query",err.stack);
-            reject(err);
+            rej(err);
 
         }else{
             const response={
                 Message:'Success'
             }
-            resolve(response);
+            res(response);
 
         }
        
     })})}
 
+//Database query to post a book.
 
 
 exports.postBooks=(title, author, genre, availability, isbn)=>{
 
-    return new Promise((resolve, reject) => {
+    return new Promise((res, rej) => {
 
 
     client.query('INSERT into library (title, author, genre, availability, isbn) values($1,$2,$3,$4,$5)',[title,author,genre,availability,isbn], (err,resp)=>{
         if(err){
             console.error("Error executing query",err.stack);
-            reject(err);
+            rej(err);
 
         }else{
             const response={
@@ -114,7 +106,7 @@ exports.postBooks=(title, author, genre, availability, isbn)=>{
                 availability:availability ,
                 isbn: isbn
             }
-            resolve(response);
+            res(response);
             console.log(response)
 
         }
